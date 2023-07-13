5040d1fb3c4c98e438799b2408e05b8022d367be
Phuoc Tai <phuoctai12t@gmai.com>
2023-07-13 11:08:43 +0700

    scan

--- src/screens/home/index.tsx
+++ src/screens/home/index.tsx
@@ -1,8 +1,165 @@
-import {StyleSheet, Text, View} from 'react-native';
-import React from 'react';
+import TetheringManager from '@react-native-tethering/wifi';
+import {Text} from '@rneui/themed';
+import Search from 'assets/svgs/search.svg';
+import Setting from 'assets/svgs/setting.svg';
+import LoadingModal from 'components/atoms/loading-modal';
+import React, {FC, useState} from 'react';
+import {useTranslation} from 'react-i18next';
+import {
+  Alert,
+  Linking,
+  PermissionsAndroid,
+  Platform,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+import {SvgProps} from 'react-native-svg';
+import WifiManager from 'react-native-wifi-reborn';
+import {MainTabScreenProps} from 'typings/navigation';
+import {IconSizes} from 'utils';
+import AppStyles from 'utils/styles';
+import {Colors} from 'utils/themes';
 
-export default function HomeScreen() {
-  return <View></View>;
+type ItemProps = {
+  Icon: FC<SvgProps>;
+  title: string;
+  onPress: () => void;
+};
+
+const Item = ({Icon, title, onPress}: ItemProps) => {
+  return (
+    <TouchableOpacity
+      style={[AppStyles.flex1, AppStyles.itemCenter]}
+      onPress={onPress}>
+      <Icon
+        width={IconSizes.veryLarge}
+        height={IconSizes.veryLarge}
+        color={Colors.primary}
+        style={AppStyles.marginBottomSmall}
+      />
+      <Text>{title}</Text>
+    </TouchableOpacity>
+  );
+};
+
+const ssid = 'F 5G';
+const password = '999999999';
+const url = 'http://192.168.0.1';
+const checkCurrentSSIDIntervalTime = 2000;
+const maxCheckCurrentSSIDIntervalTimes = 5;
+
+export default function HomeScreen({navigation}: MainTabScreenProps<'Home'>) {
+  const {t} = useTranslation();
+  const [loading, setLoading] = useState(false);
+
+  const onPressScan = async () => {
+    setLoading(true);
+    if (Platform.OS === 'android') {
+      const granted = await PermissionsAndroid.request(
+        PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+      );
+      if (granted === PermissionsAndroid.RESULTS.GRANTED) {
+        try {
+          try {
+            const currentSSID = await WifiManager.getCurrentWifiSSID();
+            if (currentSSID === ssid) {
+              setLoading(false);
+              Linking.openURL(url);
+              return;
+            }
+          } catch (error) {}
+
+          const isWifiEnabled = await TetheringManager.isWifiEnabled();
+          if (!isWifiEnabled) {
+            // TODO: if user don't press enable wifi button,
+            // next code will bot be executed,
+            // except next time user press enable button, this (previous) code will be executed,
+
+            /** In android 10 and above, you cannot enable WiFi programmatically,
+             * so a popup will be shown to the user to enable it manually.
+             * If user don't press enable button, next code will not be executed,
+             * except next time user press enable wifi button,
+             * this (previous) code will be executed,
+             * so we need hide loading modal here
+             */
+            setLoading(false);
+            await TetheringManager.setWifiEnabled();
+          }
+          /** Contiunue show loading modal if user press enable wifi button */
+          setLoading(true);
+          // TODO: if new network was saved and has current network,
+          // can not disconnect from current network
+
+          /** Need disconnect from current network to connect to new network */
+          await TetheringManager.disconnectFromNetwork();
+          await TetheringManager.connectToNetwork({
+            ssid,
+            password,
+          });
+          try {
+            /** If user don't press save button, next code will not be executed,
+             * so we need hide loading modal here
+             */
+            setLoading(false);
+            await TetheringManager.saveNetworkInDevice({
+              ssid,
+              password,
+            });
+          } catch (error) {}
+          /** Contiunue show loading modal if user press save wifi button */
+          setLoading(true);
+
+          let checkCurrentSSIDIntervalTimes = 0;
+          const checkCurrentSSIDInterval = setInterval(async () => {
+            if (
+              checkCurrentSSIDIntervalTimes >= maxCheckCurrentSSIDIntervalTimes
+            ) {
+              setLoading(false);
+              clearInterval(checkCurrentSSIDInterval);
+              Alert.alert(t('util.error'), t('alert.error.default'));
+              return;
+            } else {
+              checkCurrentSSIDIntervalTimes++;
+              try {
+                const currentSSID = await WifiManager.getCurrentWifiSSID();
+                setLoading(false);
+                clearInterval(checkCurrentSSIDInterval);
+                if (currentSSID === ssid) {
+                  Linking.openURL(url);
+                } else {
+                  Alert.alert(t('util.error'), t('home.wifi_was_saved'));
+                }
+              } catch (error) {}
+            }
+          }, checkCurrentSSIDIntervalTime);
+        } catch (e) {
+          setLoading(false);
+          Alert.alert(t('util.error'), e?.toString());
+        }
+      } else {
+        setLoading(false);
+        Alert.alert(t('util.error'), t('alert.permission.location'));
+      }
+    } else {
+      setLoading(false);
+    }
+  };
+  const onPressSetting = () => {};
+
+  return (
+    <View style={[AppStyles.flex1, AppStyles.padding]}>
+      <View style={[AppStyles.row]}>
+        <Item Icon={Search} title={t('home.menu.scan')} onPress={onPressScan} />
+        <Item
+          Icon={Setting}
+          title={t('home.menu.setting')}
+          onPress={onPressSetting}
+        />
+      </View>
+      <LoadingModal isVisible={loading} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({});
